1a9a0e635cedeb8ad6bb71f10b05418e092ea72a
deepkore <kore.mitmpl2022@learner.manipal.edu>
2023-05-02 22:39:29 +0530

    Revert "signup page validation mongoose validation"
    This reverts commit 097427c9552c6cbfbf8555815bb8b22544a38088.

--- src/user/models/user.js
+++ src/user/models/user.js
@@ -1,90 +1,91 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const Session = new Schema({
-  refreshToken: {
-    type: String,
-    default: "",
-    required: true,
-  },
-});
+    refreshToken: {
+        type: String,
+        default: "",
+    },
+})
 
 const User = new Schema({
-  Name: {
-    type: String,
-    default: "",
-    required: true,
-    message: "enter name",
-  },
-  email: {
-    type: String,
-    default: "",
-    required: true,
-    message: "enter email",
-  },
-  phone: {
-    type: String,
-    default: "",
-  },
-  gender: {
-    type: String,
-    default: "",
-  },
-  maritalStatus: {
-    type: String,
-    default: "",
-  },
-  state: {
-    type: String,
-    default: "",
-  },
-  residence: {
-    type: String,
-    default: "",
-  },
-  category: {
-    type: String,
-    default: "",
-  },
-  diferentlyAbled: {
-    type: String,
-    default: "",
-  },
-  minority: {
-    type: String,
-    default: "",
-  },
-  student: {
-    type: String,
-    default: "",
-  },
-  bpl: {
-    type: String,
-    default: "",
-  },
-  familyIncome: {
-    type: Number,
-    default: "",
-  },
-  parentIncome: {
-    type: String,
-    default: "",
-  },
-  refreshToken: {
-    type: [Session],
-  },
-});
+    firstName: {
+        type: String,
+        default: "",
+    },
+    lastName: {
+        type: String,
+        default: "",
+    },
+    email: {
+        type: String,
+        default: ""
+    },
+    phone: {
+        type: String,
+        default: ""
+    },
+    gender: {
+        type: String,
+        default: ""
+    },
+    maritalStatus: {
+        type: String,
+        default: "",
+    },
+    state: {
+        type: String,
+        default: "",
+    },
+    residence: {
+        type: String,
+        default: "",
+    },
+    category: {
+        type: String,
+        default: ""
+    },
+    diferentlyAbled: {
+        type: String,
+        default: ""
+    },
+    minority: {
+        type: String,
+        default: ""
+    },
+    student: {
+        type: String,
+        default: "",
+    },
+    bpl: {
+        type: String,
+        default: "",
+    },
+    familyIncome: {
+        type: Number,
+        default: "",
+    },
+    parentIncome: {
+        type: String,
+        default: ""
+    },
+    refreshToken: {
+        type: [Session],
+    },
+
+}
+)
 
 //Remove refreshToken from the response
 User.set("toJSON", {
-  transform: function (doc, ret, options) {
-    delete ret.refreshToken;
-    return ret;
-  },
-});
+    transform: function (doc, ret, options) {
+        delete ret.refreshToken
+        return ret
+    },
+})
 
-User.plugin(passportLocalMongoose);
+User.plugin(passportLocalMongoose)
 
-module.exports = mongoose.model("User", User);
+module.exports = mongoose.model("User", User)
--- src/user/routes/Routes.js
+++ src/user/routes/Routes.js
@@ -1,108 +1,97 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/user");
-const passport = require("passport");
+const express = require("express")
+const router = express.Router()
+const User = require("../models/user")
+const passport = require("passport")
 
-const {
-  getToken,
-  COOKIE_OPTIONS,
-  getRefreshToken,
-  verifyUser,
-} = require("../../authen/authenticate");
+const { getToken, COOKIE_OPTIONS, getRefreshToken,verifyUser } = require("../../authen/authenticate")
 
 router.post("/signup", (req, res, next) => {
-  // Verify that first name is not empty
-<<<<<<< HEAD
-  const name = req.body.name;
-  var nameMatch = req.body.email.match(/^([^@]*)@/);
-  var username = nameMatch ? nameMatch[1] : null;
-=======
-  const Name = req.body.Name;
-  const nameMatch = req.body.email.match(/^([^@]*)@/);
-  const username = nameMatch ? nameMatch[1] : null;
-
->>>>>>> parent of f8cbab5 (Merge pull request #2 from deepkore/filter)
-  console.log(req.body);
-  if (!name) {
-    res.statusCode = 500;
-    res.send({
-      name: "FirstNameError",
-      message: "The first name is required",
-    });
-  } else {
-    User.register(
-      new User({
-        username: username,
-        email: req.body.email,
-        firstName: name,
-        phone: req.body.phoneNumber,
-      }),
-      req.body.password,
-      (err, user) => {
-        console.log(err);
-        console.log(user);
-        if (err) {
-          res.status(500).json({ success: false, err: err });
-        } else {
-          user.firstName = name;
-          user.lastName = "";
-          const token = getToken({ _id: user._id });
-          const refreshToken = getRefreshToken({ _id: user._id });
-          user.refreshToken.push({ refreshToken });
-          user.save().then((user, err) => {
-            if (err) {
-              res.status(500).json({ success: false, err: err });
-            } else {
-              res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
-              res.status(200).json({ success: true, token: token });
-            }
-          });
-        }
-      }
-    );
-  }
-});
-
-console.log(req.body);
-// if (!firstName) {
-//   res.statusCode = 500;
-//   res.send({
-//     name: "FirstNameError",
-//     message: "The first name is required",
-//   });
-// } else {
-User.register(
-  new User({
-    username: username,
-    email: req.body.email,
-    Name: Name,
-    phone: req.body.phone,
-  }),
-  req.body.password,
-  (err, user) => {
-    console.log(err);
-    console.log(user);
-    if (err) {
-      res.status(500).json({ status: "not registered", err: err });
+    // Verify that first name is not empty
+    const name = req.body.name;
+    var nameMatch = req.body.email.match(/^([^@]*)@/);
+    var username = nameMatch ? nameMatch[1] : null;
+        console.log(req.body);
+    if (!name) {
+        res.statusCode = 500
+        res.send({
+            name: "FirstNameError",
+            message: "The first name is required",
+        })
     } else {
-      //    user.firstName = firstName;
-      //  user.lastName = req.body.lastName;
-      console.log("user register");
-      const token = getToken({ _id: user._id });
-      const refreshToken = getRefreshToken({ _id: user._id });
-      user.refreshToken.push({ refreshToken });
-      user.save().then((user, err) => {
-        if (err) {
-          res.status(500).json({ status: "not saved successfully", err: err });
-        } else {
-          res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
-          res.status(200).json({ success: true, token: token });
-        }
-        //  res.redirect("/forms");
-      });
+        User.register(
+            new User({ username: username , email : req.body.email, firstName : name,phone : req.body.phoneNumber}),
+            req.body.password,
+            (err, user) => {
+                console.log(err);
+                console.log(user);
+                if (err) {
+                    res.status(500).json({success : false, err : err});
+                } else {
+                    user.firstName = name
+                    user.lastName = ""
+                    const token = getToken({ _id: user._id })
+                    const refreshToken = getRefreshToken({ _id: user._id })
+                    user.refreshToken.push({ refreshToken })
+                    user.save().then((user, err) => {
+                        if (err) {
+                            res.status(500).json({success: false,err : err});
+                        } else {
+                         res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS)
+                            res.status(200).json({ success: true, token : token});
+                        }
+                    });
+                }
+            }
+        )
     }
-  }
-);
+})
+
+
+
+
+  console.log(req.body);
+  // if (!firstName) {
+  //   res.statusCode = 500;
+  //   res.send({
+  //     name: "FirstNameError",
+  //     message: "The first name is required",
+  //   });
+  // } else {
+  User.register(
+    new User({
+      username: username,
+      email: req.body.email,
+      Name: Name,
+      phone: req.body.phone,
+    }),
+    req.body.password,
+    (err, user) => {
+      console.log(err);
+      console.log(user);
+      if (err) {
+        res.status(500).json({ status: "not registered", err: err });
+      } else {
+        //    user.firstName = firstName;
+        //  user.lastName = req.body.lastName;
+        console.log("user register");
+        const token = getToken({ _id: user._id });
+        const refreshToken = getRefreshToken({ _id: user._id });
+        user.refreshToken.push({ refreshToken });
+        user.save().then((user, err) => {
+          if (err) {
+            res
+              .status(500)
+              .json({ status: "not saved successfully", err: err });
+          } else {
+            res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
+            res.status(200).json({ success: true, token: token });
+          }
+          //  res.redirect("/forms");
+        });
+      }
+    }
+  );
+});
 
 router.post("/login", passport.authenticate("local"), (req, res, next) => {
   console.log(req.body);
@@ -118,45 +107,49 @@ router.post("/login", passport.authenticate("local"), (req, res, next) => {
           res.status(200).json({ success: true, token });
         })
         .catch((err) => {
-          res.status(500).json({ success: false, err: err });
+          res.status(500).json({ message: "token not refreshed ", err: err });
         });
-    })
-    .catch((err) => {
+
+      },
+     
+    ).catch(err=>{
       console.log(err);
-      res.status(500).json({ success: false, err: err });
+      res
+        .status(500)
+        .json({ status: "cannot find user name sign up", err: err });
     });
 });
 
-router.get("/logout", verifyUser, (req, res, next) => {
-  const { signedCookies = {} } = req;
-  const { refreshToken } = signedCookies;
-  User.findById(req.user._id)
-    .then((user) => {
-      const tokenIndex = user.refreshToken.findIndex(
-        (item) => item.refreshToken === refreshToken
-      );
-
-      if (tokenIndex !== -1) {
-        user.refreshToken.id(user.refreshToken[tokenIndex]._id).remove();
-      }
-
-      user
-        .save()
-        .then((user) => {
-          res.clearCookie("refreshToken", COOKIE_OPTIONS);
-          res.status(200).json({ success: true });
-        })
-        .catch((err) => {
-          res.status(500).json({ success: false, err: err });
+  router.get("/logout", verifyUser, (req, res, next) => {
+    const { signedCookies = {} } = req
+    const { refreshToken } = signedCookies
+    User.findById(req.user._id).then(
+      user => {
+        const tokenIndex = user.refreshToken.findIndex(
+          item => item.refreshToken === refreshToken
+        )
+  
+        if (tokenIndex !== -1) {
+          user.refreshToken.id(user.refreshToken[tokenIndex]._id).remove()
+        }
+  
+        user.save().then((user) => {
+          
+            res.clearCookie("refreshToken", COOKIE_OPTIONS)
+            res.status(200).json({ success: true })
+          
+        }).catch(err => {
+          res.status(500).json({success : false ,err : err})
         });
+      },
+     
+    ).catch(err => {
+      res.status(500).json({success : false ,err : err})
     })
-    .catch((err) => {
-      res.status(500).json({ success: false, err: err });
-    });
-});
+  })
 
-router.get("/user", verifyUser, (req, res, next) => {
-  res.status(200).json({ success: true, user: req.user });
-});
+  router.get("/user", verifyUser, (req, res, next) => {
+    res.status(200).json({success : true, user : req.user});
+  })
 
-module.exports = router;
+module.exports = router
